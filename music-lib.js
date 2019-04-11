b0209b53d00a732ced2eae9dc63e82baa000f93d
karen1au <kagemikaka@hotmail.com>
2019-04-11 19:29:00 +0000

    modified into using prototype

--- music-lib.js
+++ music-lib.js
@@ -1,50 +1,44 @@
-class Library {
-    constructor (name, creator){
-        this.name = name;
-        this.creator = creator;
-        this.playLists = [];
-
-    }
-    
-    set newPlayList (list){
-        this.playLists.push(list)
-    }
+const Library = function(name, creator){
+    this.name = name;
+    this.creator = creator;
+    this.playLists = [];
 }
 
-class Playlist {
-    constructor (name) {
-        this.name = name;
-        this.tracks = [];
-    }
+Library.prototype.newPlayList = function(list){
+    this.playLists.push(list)
+}
 
-    set newTracks (track){
-        this.tracks.push(track)
-    }
-    get overallRating (){
-        return (this.tracks.map( track => {
-            return track.rating
-        }).reduce( (a,b) => { return a + b}) ) / this.tracks.length;
-    }
-    get totalDuration (){
+const Playlist = function(name) {
+    this.name = name;
+    this.tracks = [];
+}
+
+Playlist.prototype.newTracks = function(track){
+    this.tracks.push(track)
+}
+Playlist.prototype.overallRating = function(){
+    return (this.tracks.map( track => {
+        return track.rating
+    }).reduce( (a,b) => { return a + b}) ) / this.tracks.length;
+}
+Playlist.prototype.totalDuration = function() {
         return this.tracks.map( track => {
                 return track.length
                 }).reduce( (a,b) => { return a + b })
-    }
 }
 
-class Track {
-    constructor (title,rating,length) {
-        this.title = title;
-        this.rating = rating;
-        this.length = length;
-    }
+
+const Track = function(title,rating,length) {
+    this.title = title;
+    this.rating = rating;
+    this.length = length;
 }
 
 const abc = new Track("abc",3,120);
 const children = new Playlist('children');
-children.newTracks = abc
+children.newTracks(abc);
 const efg = new Track("efg",5,100);
-children.newTracks = efg
+children.newTracks(efg);
 const myLib = new Library("first","me")
-myLib.newPlayList = children
+myLib.newPlayList(children);
 console.log(myLib)
